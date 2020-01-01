eb2a437e5122260bcdfe850613d5e27faf278067
Diego Schivo <diego.schivo@openmindonline.it>
2020-01-01 10:20:51 +0100

    todoapp

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const { Component, h, render } = window.preact;
+const html = window.htm.bind(h);
+
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
+
+const FILTERS = {
+	all: todo => true,
+	active: todo => !todo.completed,
+	completed: todo => todo.completed
+};
+
 class App extends Component {
 	constructor() {
 		super();
@@ -119,5 +131,3 @@ class App extends Component {
 		`;
 	}
 }
-
-render(h(App), document.querySelector('.todoapp'));
