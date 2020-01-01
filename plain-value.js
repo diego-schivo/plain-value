9e42c5089b9cb84169e9bbead02935e9f1ac8257
Diego Schivo <diego.schivo@openmindonline.it>
2020-01-01 09:16:48 +0100

    plain-value

--- plain-value.js
+++ plain-value.js
@@ -4,20 +4,25 @@ const { Component, h, render } = window.preact;
 const html = window.htm.bind(h);
 
 
-/** Example classful component */
 class App extends Component {
 	componentDidMount() {
 		this.setState({ message:'Hello!' });
+		console.log(1, this);
+
+		var docRef = firestore.collection("users").doc("iN50PPzc8PkmAkWSG8X4");
+		docRef.get().then(function(doc) {
+			 console.log(2, this);
+			 if (doc.exists) {
+				  console.log("Document data:", doc.data());
+			 } else {
+				  // doc.data() will be undefined in this case
+				  console.log("No such document!");
+			 }
+		}).catch(function(error) {
+			 console.log("Error getting document:", error);
+		});
 	}
 	render(props, state) {
-		/*
-		return (
-			h('div', {id:'app'},
-				h(Header, { message: state.message }),
-				h(Main)
-			)
-		);
-		*/
 		return html`
 			<div id="app">
 				<${Header} message=${state.message}><//>
@@ -27,14 +32,7 @@ class App extends Component {
 	}
 }
 
-/** Components can just be pure functions */
 const Header = (props) => {
-	/*
-	return h('header', null,
-		h('h1', null, 'App'),
-		props.message && h('h2', null, props.message)
-	);
-	*/
 	return html`
 		<header>
 			<h1>App</h1>
@@ -44,20 +42,12 @@ const Header = (props) => {
 };
 
 
-/** Instead of JSX, use: h(type, props, ...children) */
 class Main extends Component {
 	render() {
 		const items = [1,2,3,4,5].map( (item) => (
 			// h('li', {id:item}, 'Item '+item)
 			html`<li id="${item}">Item ${item}</li>`
 		));
-		/*
-		return (
-			h('main', null,
-				h('ul', null, items)
-			)
-		);
-		*/
 		return html`
 			<main>
 				${html`<ul>${items}</ul>`}
